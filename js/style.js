846e5abe86f00237cf4654cb1bc032dd5baf1314
JessyJamesPasay20 <93660089+JessyJamesPasay20@users.noreply.github.com>
2021-11-03 14:39:31 +0100

    Initial commit

---
+++ js/style.js
@@ -0,0 +1,15 @@
+// logic for the hamburger menu in the nav. 
+function openSlideMenu(){
+    document.getElementsByTagName('header')[0].style.alignItems = 'unset';
+    document.getElementsByTagName('header')[0].style.padding = '0';
+    document.getElementsByTagName('nav')[0].style.width = '100%';
+    document.getElementsByTagName('nav')[0].style.display = 'flex';
+  }
+  function closeSlideMenu(){
+    document.getElementsByTagName('header')[0].style.alignItems = 'center';
+    document.getElementsByTagName('header')[0].style.padding = '20px 5%';
+    document.getElementsByTagName('nav')[0].style.width = '0';
+    document.getElementsByTagName('nav')[0].style.display = 'none';
+    
+}
+//end
