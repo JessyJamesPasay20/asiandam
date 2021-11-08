73a7caf38eeb722026d0069b58400c3f3518183f
JessyJamesPasay20 <93660089+JessyJamesPasay20@users.noreply.github.com>
2021-11-08 23:42:49 +0100

    update v3
    Big ui / ux changes

--- js/style.js
+++ js/style.js
@@ -11,8 +11,19 @@ function openSlideMenu(){
     document.getElementsByTagName('nav')[0].style.width = '0';
     document.getElementsByTagName('nav')[0].style.display = 'none';
   }
-  // scroll view 
-function scrollview(){
-  var scrollview = document.querySelector('.scrollview');
-  var scrollviewposition = scrollview.getBoundingClientRect().top;
+  
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    document.getElementsByTagName('header').style.position = "fixed";
+    document.getElementsByTagName('header').style.background = "#e6e6e6f0";
+    document.getElementsByTagName('header').style.clipPath ="polygon(0 0, 100% 0, 100% 100%, 70% 100%, 50% 70%, 30% 100%, 0 100%)";
+  }
+  else {
+    document.getElementsByTagName('header').style.position = "relative";
+    document.getElementsByTagName('header').style.background = "transparent";
+    document.getElementsByTagName('header').style.clipPath ="none";
+    
+  }
 }
